3f94f6cd2bdff62f94ffc26f5f4ed93434e69eff
Abhilash CS <abhilash.cs@webandcrafts.com>
2024-05-31 18:16:27 +0530

    Added a copy code button to the CodeBlock component with a clipboard icon

--- components/CodeBlock/index.js
+++ components/CodeBlock/index.js
@@ -396,6 +396,10 @@ ${colorData
     },
   };
 
+const handleCopyCode = (textToCopy) => {
+  navigator.clipboard.writeText(textToCopy)
+}
+  
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
@@ -436,6 +440,34 @@ ${colorData
         >
           {Object.values(categories).map((posts, idx) => (
             <Tab.Panel key={idx}>
+              <button className="fixed bottom-6 right-6 z-10 bg-[#1a2734] size-12 rounded-full grid place-items-center hover:scale-110 transition-transform duration-300 ease-in-out outline-none" onClick={() => handleCopyCode(posts.text)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="21.452"
+                  viewBox="0 0 20 21.452"
+                  fill="#fff"
+                >
+                  <g
+                    id="Group_1"
+                    data-name="Group 1"
+                    transform="translate(-829 -511)"
+                  >
+                    <path
+                      id="Path_1"
+                      data-name="Path 1"
+                      d="M20.571,35H9.714A1.707,1.707,0,0,0,8,36.714V47.6A1.722,1.722,0,0,0,9.714,49.31H20.6A1.722,1.722,0,0,0,22.31,47.6V36.714A1.743,1.743,0,0,0,20.571,35Zm.286,12.571a.264.264,0,0,1-.262.262H9.714a.264.264,0,0,1-.262-.262V36.714a.264.264,0,0,1,.262-.262H20.6a.264.264,0,0,1,.262.262Z"
+                      transform="translate(821 483.143)"
+                    />
+                    <path
+                      id="Path_2"
+                      data-name="Path 2"
+                      d="M44.571,5H33.714A1.707,1.707,0,0,0,32,6.714v4h1.429v-4a.264.264,0,0,1,.262-.262H44.571a.264.264,0,0,1,.262.262V17.6a.264.264,0,0,1-.262.262H42v1.429h2.571a1.722,1.722,0,0,0,1.714-1.714V6.714A1.707,1.707,0,0,0,44.571,5Z"
+                      transform="translate(802.714 506)"
+                    />
+                  </g>
+                </svg>
+              </button>
               <CodeMirror
                 value={posts.text}
                 height="auto"
